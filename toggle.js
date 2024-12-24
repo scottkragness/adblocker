a86e9c47d9a0d9f0b96259b7012fc097737d6a19
Scott <kragnessscott024@gmail.com>
2024-12-24 12:29:03 -0800

    First commit

---
+++ toggle.js
@@ -0,0 +1,15 @@
+document.getElementById('toggleOn').addEventListener('click', async () => {
+    chrome.declarativeNetRequest.updateEnabledRulesets({
+        disableRulesetIds: [], 
+        enableRulesetIds: ["ruleset"]
+      });
+      
+});
+
+document.getElementById('toggleOff').addEventListener('click', async () => {
+    chrome.declarativeNetRequest.updateEnabledRulesets({
+        disableRulesetIds: ["ruleset"], 
+        enableRulesetIds: []
+      });
+      
+});
